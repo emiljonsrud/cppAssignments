14d1847661014d5ee09e5f2f6b7c87839ff84e2c
Emil <emiljonsrud@gmail.com>
2022-04-10 21:29:54 +0200

    Added all the TetrominoTypes to the matrixMap

--- ov12/Tetromino.cpp
+++ ov12/Tetromino.cpp
@@ -1,35 +1,61 @@
 #include "Tetromino.h"
     
 const map<TetrominoType, vector<vector<int>>> initialMatrixMap {
-
-    {
+{
     TetrominoType::J,
-        {
+    {
         {0, 0, 0},
         {1, 1, 1},
         {0, 0, 1}
-        }
-    },
-    { 
+    }
+}, { 
     TetrominoType::L, 
-        {
+    {
         {0, 0, 0},
         {1, 1, 1},
         {1, 0, 0}
-        }
-    },
-    {
-    TetrominoType::I, 
-        {
-        //Denne er representert som en 6x6 matrise for å få en bedre rotasjonsfunksjonalitet
-        {0, 0, 1, 0, 0, 0},
-        {0, 0, 1, 0, 0, 0},
-        {0, 0, 1, 0, 0, 0},
-        {0, 0, 1, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0}
-        }
     }
+}, {
+    TetrominoType::T,
+    {
+        {0, 0, 0},
+        {1, 1, 1},
+        {0, 1, 0}
+    }
+}, {
+    TetrominoType::S,
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 1},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0}
+    }
+}, {
+    TetrominoType::Z,
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 1},
+        {0, 0, 0, 0},
+    }
+}, {
+    TetrominoType::O,
+    {
+        {1, 1},
+        {1, 1}
+    }
+}, {
+    TetrominoType::I, 
+    {
+    //Denne er representert som en 6x6 matrise for å få en bedre rotasjonsfunksjonalitet
+    {0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0}
+    }
+}
 };
 
 Tetromino::Tetromino(Point startingPoint, TetrominoType tetType) : 
@@ -87,4 +113,10 @@ bool Tetromino::blockExist(int row, int column) const {
     if(row >= matrixSize || column >= matrixSize) {return false;}
     else if(blockMatrix.at(row).at(column) == TetrominoType::NONE) {return false;}
     else{return true;}
+}
+TetrominoType Tetromino::getBlock(int row, int column) {
+    if(blockExist(row, column)) {
+        return blockMatrix.at(row).at(column);
+    } else {return TetrominoType::NONE;}
+    
 }
